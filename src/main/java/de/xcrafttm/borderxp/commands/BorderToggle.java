56a7fd70153cd965b44aa1ee3554821cd05cad84
XCraftTM <erikselesnev31@gmail.com>
2021-09-27 20:05:31 +0200

    Changed Toggle Message

--- src/main/java/de/xcrafttm/borderxp/commands/BorderToggle.java
+++ src/main/java/de/xcrafttm/borderxp/commands/BorderToggle.java
@@ -22,8 +22,8 @@ public class BorderToggle implements CommandExecutor {
         if (commandSender instanceof Player) {
             Main.isEnabled = !Main.isEnabled;
             if (Main.isEnabled) {
-                commandSender.sendMessage("§8[§6BorderXP§8] §6Challenge §aactivated!");
-            } else commandSender.sendMessage("§8[§6BorderXP§8] §6Challenge §cDeactivated!");
+                commandSender.sendMessage("§8[§6BorderXP§8] §6Challenge §aEnabled!");
+            } else commandSender.sendMessage("§8[§6BorderXP§8] §6Challenge §cDisabled!");
         }
         return true;
     }
