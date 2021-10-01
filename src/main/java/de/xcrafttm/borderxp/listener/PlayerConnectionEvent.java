38a377b059cfdbc23373c8a2f0ac4ddd11321762
XCraftTM <erikselesnev31@gmail.com>
2021-10-01 16:11:38 +0200

    Release 1.3

--- src/main/java/de/xcrafttm/borderxp/listener/PlayerConnectionEvent.java
+++ src/main/java/de/xcrafttm/borderxp/listener/PlayerConnectionEvent.java
@@ -25,28 +25,33 @@ public class PlayerConnectionEvent implements Listener {
     @EventHandler
     public void onFirstJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        World world = Bukkit.getWorld("world");
-        Location village = player.getWorld().locateNearestStructure(player.getLocation(), StructureType.VILLAGE, 5000, true);
-        if (village == null) {
-            player.sendMessage("§8[§6BorderXP§8] §cNo Village was found, to spawn you in. Please Reset the World and Restart!");
-            return;
-        }
-        Location loc = player.getWorld().getHighestBlockAt(village).getLocation().add(0.5, 1, 0.5);
-        village.getChunk().load(true);
-
         if (!player.hasPlayedBefore()) {
-        world.setSpawnLocation(loc);
-        player.teleport(loc);
-        WorldBorder border =  Objects.requireNonNull(Bukkit.getWorld("world")).getWorldBorder();
-        WorldBorder netherborder = Objects.requireNonNull(Bukkit.getWorld("world_nether")).getWorldBorder();
-        WorldBorder endborder = Objects.requireNonNull(Bukkit.getWorld("world_the_end")).getWorldBorder();
-        border.setCenter(loc.getBlockX() + 0.5, loc.getBlockZ() + 0.5);
-        border.setSize(1);
+            World world = Bukkit.getWorld("world");
+            Location village = player.getWorld().locateNearestStructure(player.getLocation(), StructureType.VILLAGE, 5000, true);
+            if (village == null) {
+                player.sendMessage("§8[§6BorderXP§8] §cNo Village was found, to spawn you in. Please Reset the World and Restart!");
+                return;
+            }
+            Location loc = player.getWorld().getHighestBlockAt(village).getLocation().add(0.5, 1, 0.5);
+            village.getChunk().load(true);
 
-        netherborder.setCenter(Objects.requireNonNull(Bukkit.getWorld("world_nether")).getSpawnLocation());
-        endborder.setCenter(Objects.requireNonNull(Bukkit.getWorld("world_the_end")).getSpawnLocation());
+            world.setSpawnLocation(loc);
+            player.teleport(loc);
+            WorldBorder border = Objects.requireNonNull(Bukkit.getWorld("world")).getWorldBorder();
+            WorldBorder netherborder = Objects.requireNonNull(Bukkit.getWorld("world_nether")).getWorldBorder();
+            WorldBorder endborder = Objects.requireNonNull(Bukkit.getWorld("world_the_end")).getWorldBorder();
+            border.setCenter(loc.getBlockX() + 0.5, loc.getBlockZ() + 0.5);
+            border.setSize(1);
+
+            netherborder.setCenter(Objects.requireNonNull(Bukkit.getWorld("world_nether")).getSpawnLocation());
+            endborder.setCenter(Objects.requireNonNull(Bukkit.getWorld("world_the_end")).getSpawnLocation());
 
             player.setLevel(1);
+        }
+
+        WorldBorder border = Objects.requireNonNull(Bukkit.getWorld("world")).getWorldBorder();
+        WorldBorder netherborder = Objects.requireNonNull(Bukkit.getWorld("world_nether")).getWorldBorder();
+        WorldBorder endborder = Objects.requireNonNull(Bukkit.getWorld("world_the_end")).getWorldBorder();
 
             AtomicInteger maxXP = new AtomicInteger(1);
             Bukkit.getOnlinePlayers().forEach(player1 -> {
@@ -54,8 +59,8 @@ public class PlayerConnectionEvent implements Listener {
                     maxXP.set((int) player1.getLevel());
             });
             border.setSize(maxXP.get());
-            netherborder.setSize(maxXP.get(), 5);
-            endborder.setSize(maxXP.get(), 5);
-        }
+            netherborder.setSize(maxXP.get());
+            endborder.setSize(maxXP.get());
+
     }
 }
